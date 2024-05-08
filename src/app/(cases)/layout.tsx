569699370b9fde9ca5f93c411ef6e80f19ee1543
andressaaborges <emylleaborges@gmail.com>
2024-05-07 22:28:45 -0300

    feat: :sparkles: add basic api

--- src/app/(cases)/layout.tsx
+++ src/app/(cases)/layout.tsx
@@ -1,7 +1,7 @@
 import { Header } from '@/components/header'
 import { ReactNode } from 'react'
 
-export default function HomeLayout({ children }: { children: ReactNode }) {
+export default function CasesLayout({ children }: { children: ReactNode }) {
   return (
     <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 p-8">
       <Header />
